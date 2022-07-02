a3322abaddfac8f7fbd164df5abe2240ead56f66
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-07-02 15:17:45 +0600

    ex1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,12 @@ const Heading = ({ text }) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return (<>{text} {value}</>)
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value} {text === "positive" && "%"}</td>
+    </tr>
+  );
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -19,14 +24,16 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = good * 100 / all;
 
   return (
-    <p>
-      <StatisticLine text="good" value={good} /><br />
-      <StatisticLine text="neutral" value={neutral} /><br />
-      <StatisticLine text="bad" value={bad} /><br />
-      <StatisticLine text="all" value={all} /><br />
-      <StatisticLine text="average" value={average} /><br />
-      <StatisticLine text="positive" value={positive} /> %
-    </p>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   );
 }
 
